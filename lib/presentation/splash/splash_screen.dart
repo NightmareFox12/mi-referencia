50707c24954f5c112ca8ded5b6e0308c008edaa5
NightmareFox12 <carloshenriquez392004@gmail.com>
2025-10-21 20:40:33 -0400

    Replace logo.jpg with logo.png and update splash screen layout

--- lib/presentation/splash/splash_screen.dart
+++ lib/presentation/splash/splash_screen.dart
@@ -7,7 +7,21 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(child: Image.asset('assets/images/logo.png')),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              spacing: 6,
+              children: [
+                Image.asset('assets/images/logo.png', width: 100, height: 100),
+                Text(
+                  'Mi Referencia',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }

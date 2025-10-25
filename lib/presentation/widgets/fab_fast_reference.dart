09fad3554379a7a870867f8ac3165c1a93e2d90f
NightmareFox12 <carloshenriquez392004@gmail.com>
2025-10-24 21:38:55 -0400

    feat: Refactor FabFastReference widget to improve state management and validation for reference input

--- lib/presentation/widgets/fab_fast_reference.dart
+++ lib/presentation/widgets/fab_fast_reference.dart
@@ -6,22 +6,10 @@ import 'package:mi_referencia/data/datasources/database.dart';
 class FabFastReference extends HookWidget {
   final db = AppDatabase();
 
-  //states
-  final reference = useState<int>(0);
-  final amount = useState<double>(0);
-
-  final referenceErr = useState<String?>(null);
-  final amountErr = useState<String?>(null);
-
-  validateReference(String value) {
-    if (value.isEmpty) referenceErr.value = null;
-    final number = int.tryParse(value);
-
-    if (number == null)
-      referenceErr.value = 'El valor ingresado no es un número válido.';
-  }
-
-  Future<void> addFastReference() async {
+  Future<void> addFastReference(
+    ValueNotifier<int> reference,
+    ValueNotifier<double> amount,
+  ) async {
     final id = await ReferenceDataSource(
       db,
     ).setFastReference(reference.value, amount.value);
@@ -30,75 +18,91 @@ class FabFastReference extends HookWidget {
     //TODO: Meter TOAST
   }
 
-  Future<void> showFastReferenceDialog(BuildContext context) async {
+  Future<void> showFastReferenceDialog(
+    BuildContext context,
+    ValueNotifier<int> reference,
+    ValueNotifier<double> amount,
+    ValueNotifier<String?> referenceErr,
+    ValueNotifier<String?> amountErr,
+  ) async {
     return showDialog<void>(
       context: context,
       barrierDismissible: false,
       builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Referencia'),
-          content: SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                Text('Nose que poner aqui'),
-                SizedBox(height: 10),
+        return StatefulBuilder(
+          builder: (context, setState) => AlertDialog(
+            title: Text('Referencia'),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: <Widget>[
+                  Text('Nose que poner aqui'),
+                  SizedBox(height: 10),
 
-                TextFormField(
-                  keyboardType: TextInputType.number,
-                  autofocus: true,
-                  forceErrorText: referenceErr.value,
-                  decoration: const InputDecoration(
-                    // icon: Icon(Icons.person),
-                    hintText: 'Ej: 2534',
-                    labelText: 'Referencia *',
-                    border: OutlineInputBorder(),
-                  ),
-                  onChanged: (value) {
-                    reference.value = int.parse(value);
-                    validateReference(value);
-                  },
-                ),
-                SizedBox(height: 12),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    autofocus: true,
+                    forceErrorText: referenceErr.value,
+                    decoration: const InputDecoration(
+                      // icon: Icon(Icons.person),
+                      hintText: 'Ej: 2534',
+                      labelText: 'Referencia *',
+                      border: OutlineInputBorder(),
+                    ),
+                    onChanged: (value) {
+                      final parsedNumber = int.tryParse(value);
+                      if (parsedNumber != null) {
+                        reference.value = parsedNumber;
+                        referenceErr.value = null;
+                      } else {
+                        if (value.isNotEmpty)
+                          referenceErr.value = 'La referencia no es válida.';
+                      }
 
-                TextFormField(
-                  decoration: const InputDecoration(
-                    // icon: Icon(Icons.person),
-                    hintText: 'Ej: 100',
-                    labelText: 'Monto Bs.F *',
-                    suffixText: 'Bs.F',
-                    border: OutlineInputBorder(),
+                      setState(() => {});
+                    },
                   ),
-                  onSaved: (String? value) {
-                    if (value != null) amount.value = double.parse(value);
-                  },
-                  validator: (String? value) {
-                    return (value != null && value.contains('@'))
-                        ? 'Do not use the @ char.'
-                        : null;
-                  },
-                ),
-              ],
+                  SizedBox(height: 12),
+
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      // icon: Icon(Icons.person),
+                      hintText: 'Ej: 100',
+                      labelText: 'Monto Bs.F *',
+                      suffixText: 'Bs.F',
+                      border: OutlineInputBorder(),
+                    ),
+                    onSaved: (String? value) {
+                      if (value != null) amount.value = double.parse(value);
+                    },
+                    validator: (String? value) {
+                      return (value != null && value.contains('@'))
+                          ? 'Do not use the @ char.'
+                          : null;
+                    },
+                  ),
+                ],
+              ),
             ),
+            actions: <Widget>[
+              OutlinedButton.icon(
+                label: Text('Cancelar'),
+                icon: Icon(Icons.close),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+
+              FilledButton.icon(
+                label: Text('Guardar'),
+                icon: Icon(Icons.check),
+                onPressed: () => addFastReference(reference, amount),
+                // onPressed: () {
+
+                //   // Navigator.of(context).pop();
+                // },
+              ),
+            ],
           ),
-          actions: <Widget>[
-            OutlinedButton.icon(
-              label: Text('Cancelar'),
-              icon: Icon(Icons.close),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-
-            FilledButton.icon(
-              label: Text('Guardar'),
-              icon: Icon(Icons.check),
-              onPressed: () => addFastReference(),
-              // onPressed: () {
-
-              //   // Navigator.of(context).pop();
-              // },
-            ),
-          ],
         );
       },
     );
@@ -106,9 +110,21 @@ class FabFastReference extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
+    //states
+    final reference = useState<int>(0);
+    final amount = useState<double>(0);
+    final referenceErr = useState<String?>(null);
+    final amountErr = useState<String?>(null);
+
     return FloatingActionButton(
       heroTag: 'fab1',
-      onPressed: () => showFastReferenceDialog(context),
+      onPressed: () => showFastReferenceDialog(
+        context,
+        reference,
+        amount,
+        referenceErr,
+        amountErr,
+      ),
       tooltip: 'Referencia Rápida',
       child: const Icon(Icons.add),
     );

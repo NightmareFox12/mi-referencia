df9ebcce6d65280ff36a5ac6da25407326854402
NightmareFox12 <carloshenriquez392004@gmail.com>
2025-11-20 21:44:21 -0400

    feat: Implement HomeScreen with a pie chart using fl_chart and add fl_chart and equatable dependencies.

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:mi_referencia/presentation/history/history_screen.dart';
 import 'package:mi_referencia/presentation/widgets/bottom_bar_widget.dart';
 import 'package:mi_referencia/presentation/widgets/fab_menu.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 class HomeScreen extends HookWidget {
   const HomeScreen({super.key});
@@ -19,37 +20,76 @@ class HomeScreen extends HookWidget {
       body: Stack(
         alignment: Alignment.bottomRight,
         children: [
-          // ElevatedButton(
-          //   child: const Text('Show Snackbar'),
-          //   onPressed: () {
-          //     ScaffoldMessenger.of(context).showSnackBar(
-          //       SnackBar(
-          //         content: const Text('Awesome Snackbar!'),
-          //         action: SnackBarAction(
-          //           label: 'Action',
-          //           onPressed: () {
-          //             // Code to execute.
-          //           },
-          //         ),
-          //       ),
-          //     );
-          //   },
-          // ),
-          currentScreen.value == 0
-              ? HistoryScreen()
-              : Center(
-                  child: Column(
-                    children: [
-                      IconButton.filled(
-                        onPressed: () {},
-                        icon: Icon(Icons.agriculture_rounded),
+          if (currentScreen.value == 0)
+            Stack(
+              children: [
+                // 1. El Gráfico (Tu Círculo)
+                Align(
+                  alignment: Alignment.center,
+                  child: SizedBox(
+                    width: 250,
+                    height: 250,
+                    child: PieChart(
+                      PieChartData(
+                        sections: [
+                          PieChartSectionData(
+                            color: Colors.red,
+                            value: 100,
+                            radius: 12,
+                            showTitle: false,
+                          ),
+                        ],
                       ),
-                    ],
+                      duration: Duration(milliseconds: 150),
+                      curve: Curves.linear,
+                    ),
                   ),
                 ),
+
+                Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    '100%',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
+            )
+          else
+            Center(
+              child: Column(
+                children: [
+                  IconButton.filled(
+                    onPressed: () {},
+                    icon: Icon(Icons.agriculture_rounded),
+                  ),
+                ],
+              ),
+            ),
           FabMenu(currentScreen: currentScreen.value),
         ],
       ),
     );
   }
 }
+
+// ElevatedButton(
+//   child: const Text('Show Snackbar'),
+//   onPressed: () {
+//     ScaffoldMessenger.of(context).showSnackBar(
+//       SnackBar(
+//         content: const Text('Awesome Snackbar!'),
+//         action: SnackBarAction(
+//           label: 'Action',
+//           onPressed: () {
+//             // Code to execute.
+//           },
+//         ),
+//       ),
+//     );
+//   },
+// ),
